1645196f6a3d7e35bba807c581bdbf82ffd614d5
RachmanForniandi <rachmanforniandi@gmail.com>
2021-02-07 15:57:56 +0700

    item data movie done

--- app/src/main/java/com/example/moviealmanackotlin/models/MovieModel.kt
+++ app/src/main/java/com/example/moviealmanackotlin/models/MovieModel.kt
@@ -3,7 +3,11 @@ package com.example.moviealmanackotlin.models
 data class MovieModel(
     val id:Int?,
     val title:String?,
+    val vote_average:Int?,
+    val vote_count:Int?,
+    val original_language:String?,
     val backdrop_path:String?,
+    val poster_path:String?,
     val overview:String?,
     val release_date:String?,
 )
