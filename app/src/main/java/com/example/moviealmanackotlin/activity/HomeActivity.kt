9a64bd636d1b0c5579abf303a92b6d475d19231d
RachmanForniandi <rachmanforniandi@gmail.com>
2021-02-14 22:42:46 +0700

    viewBinding on home Activity done

--- app/src/main/java/com/example/moviealmanackotlin/activity/HomeActivity.kt
+++ app/src/main/java/com/example/moviealmanackotlin/activity/HomeActivity.kt
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moviealmanackotlin.R
 import com.example.moviealmanackotlin.adapters.TabAdapter
+import com.example.moviealmanackotlin.databinding.ActivityHomeBinding
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_home)
 
         val tabAdapter = TabAdapter(supportFragmentManager,lifecycle)
-        view_pager_home.adapter = tabAdapter
+        binding.viewPagerHome.adapter = tabAdapter
 
         val tabTitles = arrayOf("Popular","Now Playing")
-        TabLayoutMediator(tab_home,view_pager_home){tab,position ->
+        TabLayoutMediator(binding.tabHome,binding.viewPagerHome){tab,position ->
             tab.text =tabTitles[position]
         }.attach()
     }
